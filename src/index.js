443d3638ddb199f890712a7e844d9a7eba7f5911
Devi <deviarigi@gmail.com>
2025-11-02 22:37:20 +0530

    final commit new

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ReportWebVitals from './reportWebVitals';
+import ReportWebVitals from './components/ReportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
